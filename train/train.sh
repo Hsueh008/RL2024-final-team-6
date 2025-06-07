6939034e833723e3aaf460a8723e2c0b4fc3bf09
Hsueh008 <kevin80934@gmail.com>
2025-06-07 22:59:31 +0800

    update small modification

--- train/train.sh
+++ train/train.sh
@@ -4,5 +4,7 @@ export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 
 
-accelerate launch --num_cpu_threads_per_process 6 \
+accelerate launch \
+    --config_file train/config/acc_config.yaml \
+    --num_cpu_threads_per_process 6 \
     train/train_dpo.py \
